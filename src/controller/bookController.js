47d094b676fb83d32840b2e0a3de2582bf659c9d
khe <khj778878@naver.com>
2024-01-04 16:02:29 +0900

    [feat]: 도서 api 구현(전체 도서 목록 조회, 개별 도서 목록 조회, 카테고리 별 도서 목록 조회)

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,13 +1,65 @@
 const conn = require("../../mariadb").promise();
-const { validationResult } = require("express-validator");
 const { StatusCodes } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
+const { sqlError, serverError } = require("../utils/errorHandler");
 
 /* 전체 도서 조회 */
-const getTotalBooks = (req, res) => {};
+const getTotalBooks = async (req, res) => {
+  try {
+    const sql = "SELECT * FROM books";
+    const [results] = await conn.query(sql);
+    if (results.length > 0) {
+      return res.status(StatusCodes.OK).json(results);
+    }
+    return res.status(StatusCodes.NO_CONTENT).json({ message: "도서 목록이 비어있습니다." });
+  } catch (err) {
+    if (err.code && err.code.startsWith("ER_")) {
+      sqlError(res, err);
+    } else {
+      serverError(res, err);
+    }
+  }
+};
 
 /* 개별 도서 조회 */
-const getBookInfo = (req, res) => {};
+const getBookInfo = async (req, res) => {
+  try {
+    const { bookId } = req.params;
+
+    const sql = "SELECT * FROM books WHERE id=?";
+    const [results] = await conn.query(sql, +bookId);
+    if (results.length > 0) {
+      return res.status(StatusCodes.OK).json(results[0]);
+    }
+    return res.status(StatusCodes.NOT_FOUND).json({ message: "존재하지 않는 도서입니다." });
+  } catch (err) {
+    if (err.code && err.code.startsWith("ER_")) {
+      sqlError(res, err);
+    } else {
+      serverError(res, err);
+    }
+  }
+};
 
 /* 카테고리 별 도서 목록 조회 */
-const getCategoryBooks = (req, res) => {};
+const getCategoryBooks = async (req, res) => {
+  try {
+    const { category_id, isNew } = req.query;
+
+    const sql = "SELECT * FROM books WHERE category_id=?";
+    const [results] = await conn.query(sql, category_id);
+    if (results.length > 0) {
+      return res.status(StatusCodes.OK).json(results[0]);
+    }
+    return res
+      .status(StatusCodes.NO_CONTENT)
+      .json({ message: "해당 카테고리에 해당하는 도서가 존재하지 않습니다." });
+  } catch (err) {
+    if (err.code && err.code.startsWith("ER_")) {
+      sqlError(res, err);
+    } else {
+      serverError(res, err);
+    }
+  }
+};
+
+module.exports = { getTotalBooks, getBookInfo, getCategoryBooks };
--- src/routes/books.js
+++ src/routes/books.js
@@ -2,17 +2,11 @@ const express = require("express");
 const router = express.Router();
 router.use(express.json());
 
-/* 전체 도서 조회 */
-const getTotalBooks = (req, res) => {};
+const { getTotalBooks, getBookInfo, getCategoryBooks } = require("../controller/bookController");
+const validate = require("../middleware/validationMiddleware");
 
-/* 개별 도서 조회 */
-const getBookInfo = (req, res) => {};
-
-/* 카테고리 별 도서 목록 조회 */
-const getCategoryBooks = (req, res) => {};
-
+router.get("/", getCategoryBooks);
 router.get("/", getTotalBooks);
 router.get("/:bookId", getBookInfo);
-router.get("/", getCategoryBooks);
 
 module.exports = router;
