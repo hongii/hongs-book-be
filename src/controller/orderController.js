1e7ecbe994488e7f478485de36d33c5bb6331bbb
khe <khj778878@naver.com>
2024-01-11 21:53:14 +0900

    [update]: 주문 요청 api에서 DB에 데이터 삽입 로직에 transaction 적용

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -17,36 +17,36 @@ const requestPayment = async (req, res) => {
     // 추후, jwt토큰 유효성 검증을 통해 인증된 사용자인지 확인하는 로직 추가할 예정
     // 일단은 body로 들어오는 user_id는 항상 유효한 값이라고 가정
 
+    // 트랜잭션 시작
+    await conn.beginTransaction();
+
     let sql = "INSERT INTO deliveries (address, receiver, contact) VALUES(?, ?, ?)";
     let values = [delivery.address, delivery.receiver, delivery.contact];
     const [insertDeliveriesResults] = await conn.query(sql, values);
-    if (insertDeliveriesResults.affectedRows > 0) {
-      const deleveryId = insertDeliveriesResults.insertId;
-      sql = `INSERT INTO orders (delivery_id, user_id, main_book_title, total_price, total_quantity) 
+
+    const deleveryId = insertDeliveriesResults.insertId;
+    sql = `INSERT INTO orders (delivery_id, user_id, main_book_title, total_price, total_quantity) 
               VALUES(?, ?, ?, ?, ?)`;
-      values = [deleveryId, userId, mainBookTitle, totalPrice, totalQuantity];
-      const [insertOrdersResults] = await conn.query(sql, values);
+    values = [deleveryId, userId, mainBookTitle, totalPrice, totalQuantity];
+    const [insertOrdersResults] = await conn.query(sql, values);
 
-      if (insertOrdersResults.affectedRows > 0) {
-        const orderId = insertOrdersResults.insertId;
-        // MULTI-INSERT
-        sql = `INSERT INTO ordered_books (order_id, book_id, quantity) VALUES ?`;
-        let values = [];
-        for (let i = 0; i < items.length; i++) {
-          values.push([orderId, items[i].book_id, items[i].quantity]);
-        }
-        const [insertOrderedBooksresults] = await conn.query(sql, [values]);
-
-        if (insertOrderedBooksresults.affectedRows > 0) {
-          return res.status(StatusCodes.OK).json({ message: "주문이 완료되었습니다." });
-        }
-      }
+    const orderId = insertOrdersResults.insertId;
+    // MULTI-INSERT
+    sql = `INSERT INTO ordered_books (order_id, book_id, quantity) VALUES ?`;
+    values = [];
+    for (let i = 0; i < items.length; i++) {
+      values.push([orderId, items[i].book_id, items[i].quantity]);
     }
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({ message: "잘못된 요청입니다. 확인 후 다시 입력해주세요." });
+    await conn.query(sql, [values]);
+
+    // 트랜잭션 커밋 완료
+    await conn.commit();
+
+    return res.status(StatusCodes.OK).json({ message: "주문이 완료되었습니다." });
   } catch (err) {
     if (err.code && err.code.startsWith("ER_")) {
+      console.error("transaction rollback");
+      await conn.rollback(); // 트랜잭션 롤백
       sqlError(res, err);
     } else {
       serverError(res, err);
