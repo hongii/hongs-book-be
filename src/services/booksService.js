1894b3cf573673a29766f79f072dcc2e6a458b98
khe <khj778878@naver.com>
2024-03-03 13:20:22 +0900

    [refactor]: 도서 조회 api 수정
    - 도서 조회 시, category_id도 포함시켜서 응답 데이터 전달
    - 전체 도서 조회 및 카테고리, 신간도서 조회 시, 조회된 결과에 대한 totalBooks구하는 query를 따로 수행. 기존에는 books 테이블에 있는 전체 도서 총 개수만 구했었는데 프론트에서 조회된 결과의 총 도서수에 대한 page를 계산해야 하므로 조회된 결과에 대한 전체 도서수를 전달하도록 수정

--- src/services/booksService.js
+++ src/services/booksService.js
@@ -9,14 +9,14 @@ const RESPONSE_MESSAGES = {
 
 const getBooksInfoService = async (categoryId, isNew, page, limit) => {
   const baseSql = `SELECT b.*, c.category_name,
-                    (SELECT count(*) FROM likes WHERE likes.liked_book_id=b.id) AS likes,
-                    (SELECT count(*) FROM books) AS total_books
+                    (SELECT count(*) FROM likes WHERE likes.liked_book_id=b.id) AS likes
                   FROM books AS b INNER JOIN categories AS c USING (category_id)`;
   const categorySql = "category_id=?";
   const isNewBookSql = "published_date BETWEEN DATE_SUB(CURDATE(), INTERVAL 30 DAY) AND CURDATE()";
   const tailSql = " LIMIT ? OFFSET ?";
 
   let sql = `${baseSql} ${tailSql}`; // 전체 도서 목록 조회
+  let totalBookSql = baseSql;
   let message = RESPONSE_MESSAGES.NO_BOOKS;
   const offset = (page - 1) * limit;
   let values = [limit, offset];
@@ -34,25 +34,27 @@ const getBooksInfoService = async (categoryId, isNew, page, limit) => {
 
   if (categoryId && isNew) {
     // 카테고리 별 신간 도서 목록 조회 (출판일이 현재 일자 기준으로 30일 이내인 도서)
-    sql = `${baseSql} WHERE ${categorySql} AND ${isNewBookSql} ${tailSql}`;
+    totalBookSql = `${baseSql} WHERE ${categorySql} AND ${isNewBookSql}`;
+    sql = `${totalBookSql} ${tailSql}`;
     message = `${categoryName} 카테고리에는 ${RESPONSE_MESSAGES.NO_RECENTLY_PUBLISHED_BOOKS}`;
   } else if (categoryId && !isNew) {
     // 카테고리 별 도서 목록 조회
-    sql = `${baseSql} WHERE ${categorySql} ${tailSql}`;
+    totalBookSql = `${baseSql} WHERE ${categorySql}`;
+    sql = `${totalBookSql} ${tailSql}`;
     message = `${categoryName} 카테고리에는 ${RESPONSE_MESSAGES.NO_BOOKS}`;
   } else if (!categoryId && isNew) {
     // 신간 도서 목록 조회 (전체 카테고리 통합)
-    sql = `${baseSql} WHERE ${isNewBookSql} ${tailSql}`;
+    totalBookSql = `${baseSql} WHERE ${isNewBookSql}`;
+    sql = `${totalBookSql} ${tailSql}`;
     message = RESPONSE_MESSAGES.NO_RECENTLY_PUBLISHED_BOOKS;
   }
 
+  const [totalBookResult] = await conn.query(totalBookSql, values);
+  const totalBooks = totalBookResult.length;
+
   const [results] = await conn.query(sql, values);
   if (results.length > 0) {
-    const totalBooks = results[0].total_books;
-    let books = Object.entries(results)
-      .filter(([key]) => key !== "category_id")
-      .map((arr) => arr[1]);
-    books = snakeToCamelData(books);
+    const books = snakeToCamelData(results);
     return { data: { books, pagination: { totalBooks, page } }, message: null };
   }
 
@@ -87,11 +89,10 @@ const getBookDetailService = async (bookId, userId) => {
   }
 
   const [results] = await conn.query(sql, values);
+  console.log(results);
   if (results.length > 0) {
-    let data = Object.fromEntries(
-      Object.entries(results[0]).filter(([key]) => key !== "category_id"),
-    );
-    data = snakeToCamelData(data);
+    // let data = Object.fromEntries(Object.entries(results[0]));
+    data = snakeToCamelData(results[0]);
     return { data };
   }
   throw new CustomError(ERROR_MESSAGES.BOOKS_NOT_FOUND);
